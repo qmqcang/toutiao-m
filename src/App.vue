86726ae3491f87b1f9e5d30797035c84b3e20f4c
qmqc <2335948360@qq.com>
2021-08-11 23:35:34 +0800

    项目初始化-调整初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
+    <h1>黑马头条</h1>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="less">
 
